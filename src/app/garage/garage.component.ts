c3b74b74bce15dcb0da8ee4f3590635617636b0d
Anurag AV <Anurag>
2020-03-14 11:16:38 +0530

    Garage Module viewing and reject

--- src/app/garage/garage.component.ts
+++ src/app/garage/garage.component.ts
@@ -1,39 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
-import {Observable} from 'rxjs';
-import { log } from 'util';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-garage',
   templateUrl: './garage.component.html',
   styleUrls: ['./garage.component.css']
 })
 export class GarageComponent implements OnInit {
-  garage: AngularFirestoreCollection<any> = null;
+  garage: AngularFirestoreDocument<any> = null;
   user: AngularFirestoreDocument<any> = null;
-  items : Observable<any[]>;
-  users : Observable<any[]>;
+  items: Observable<any[]>;
+  users: Observable<any[]>;
+  vehicles = {};
+  servicedata= {};
+  customerId;
   // console.log(user);
+
+  constructor(public db: AngularFirestore) {
+    this.garage = db.collection('/garages').doc("7378956754");
+    this.garage.valueChanges().subscribe(res => {
+      console.log("garage", res)
+      this.items = res;
+      console.log(this.items["serviceRequests"])
+      for (let id in this.items["serviceRequests"]) {
+        db.collection('/users').doc(id).ref.get().then((doc) => {
+          if (doc.exists) {
+            console.log("Document data:", doc.data());
+            console.log("type", typeof doc.data());
+            console.log("vehicle data is ",doc.data().vehicles[this.items["serviceRequests"][id]["vehicle"]])
+            // console.log(doc.data().vehicles[this.items["serviceRequests"][id]["vehicle"];])
+            this.vehicles[id] = doc.data().vehicles[this.items["serviceRequests"][id]["vehicle"]];
+          } else {
+            console.log("No such document!");
+          }
+        });
+      }
+      console.log("vehicle data is", this.vehicles)
+    });
+
+  }
+  ngOnInit() {
+  }
   
-  constructor(public db: AngularFirestore){
-    this.garage = db.collection('/garages');
-   db.collection('/users').doc('7758086415').ref.get().then(function(doc)
-    {
+  viewJob = (user,vehicle)=>{
+    console.log(vehicle);
+    this.customerId = user;
+    this.db.collection('/users').doc(user).ref.get().then((doc) => {
       if (doc.exists) {
-        console.log("Document data:", doc.data());
-        console.log("type",typeof doc.data());
-        
-        // this.user = doc.data();
-        // console.log(typeof user);
+        this.servicedata = doc.data()["requestedServices"][vehicle];
+        console.log(this.servicedata)
+        // console.log(doc.data().vehicles[this.items["serviceRequests"][id]["vehicle"];])
+        // this.vehicles[id] = doc.data().vehicles[this.items["serviceRequests"][id]["vehicle"]];
       } else {
         console.log("No such document!");
       }
     });
-    // this.users = this.user.valueChanges();
-    this.items = this.garage.valueChanges();
-    // console.log(this.user)
   }
 
-  ngOnInit() {
+  rejectJob = (user)=>{
+    this.db.collection('/garages').doc("7378956754").ref.get().then((doc) => {
+      if (doc.exists) {
+        let  services = doc.data()["serviceRequests"];
+        delete services[user]
+        this.db.collection('/garages').doc("7378956754").update({serviceRequests:services})
+        // console.log(doc.data().vehicles[this.items["serviceRequests"][id]["vehicle"];])
+        // this.vehicles[id] = doc.data().vehicles[this.items["serviceRequests"][id]["vehicle"]];
+      } else {
+        console.log("No such document!");
+      }
+    });
   }
 
 }
